7ac0c9e043bc190cd24e3b8cb3ac630756e4936b
Hiroshima <excemit@gmail.com>
2019-08-30 20:29:42 +0200

    web

---
+++ main.js
@@ -0,0 +1,39 @@
+/*var array = ["Ok ", "bet ", "amigos "]
+var oneOne = (array[0]);
+for (var x=0;x<0;x++) {
+    document.write(array[0]);
+}    
+*/
+// DONT PRESS
+function baton() {
+    document.write("dx")
+};
+
+var neger = document.getElementById("zero");
+var fNigga = function() {
+    neger.innerText = "One";
+}
+var niggaOne = function() {
+    neger.innerText = "Two";
+}
+var one = document.getElementById("count")
+// COUNTER
+var number = 1;
+var counter = function() {
+      document.getElementById('number').innerHTML = number
+      number++;
+}
+//document.getElementById('buttonID').onclick = mouseclicked; ALTERNATIVE?
+
+// INPUT BOX    
+var inputBox = document.getElementById("text");
+//var aa="a";
+var a = function() {
+    //document.write(value.length);
+    document.write(inputBox.value.length);
+}
+/*for (i=0; i<7; i++) {
+    var aaaa = "a";
+   document.write(aaaa);
+   
+}*/
